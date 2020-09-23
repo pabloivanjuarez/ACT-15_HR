95a7cc10e11e854bc16ce486dacf4c1265e77f66
nosam1998 <nosam1998@gmail.com>
2020-09-22 20:04:07 -0700

    /api/tables route works

--- dev/server.js
+++ dev/server.js
@@ -15,20 +15,20 @@ app.use(express.json());
 
 app.get("/", function (res, req) {
     res.sendFile(path.join(__dirname, "home.html"));
-})
+});
 
 
 app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
-})
+});
 
 
 app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
-})
+});
 
 app.post("/reserve", function (req, res) {
-    const temp_obj = {
+    let temp_obj = {
         name: req.name,
         phone_number: req.phone_number,
         email: req.email,
@@ -39,10 +39,19 @@ app.post("/reserve", function (req, res) {
 })
 
 app.get("/api/tables", function (req, res) {
-
+    let data = fs.readFileSync('dev/reservations.json', 'utf8');
+    data = JSON.parse(data)
+    res.json(data)
 })
 
 
 app.get("/api/waitlist", function (req, res) {
 
 })
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
